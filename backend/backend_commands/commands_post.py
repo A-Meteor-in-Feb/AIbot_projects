64d7f5cc041931fc484d68c97feef35fbd2d8e6e
yangtianjiao <yang.diandian@hotmail.com>
2025-07-16 10:35:19 +0800

    specific commands added

--- backend/backend_commands/commands_post.py
+++ backend/backend_commands/commands_post.py
@@ -1,16 +1,37 @@
 import requests
 import json
 
+
+COMMAND_MOVE = '0'
+COMMAND_DELIVER = '1'
+COMMAND_PAUSE = '2'
+COMMAND_RESUME = '3'
+COMMAND_ABORT = '4'
+
+RUNNING = True
+
+def on_quit():
+    global RUNNING
+    print("The program is exiting ...")
+    RUNNING = False
+
+
 def send_command(base_url, token, command, robotId, parameters):
     url = f"{base_url}/api/robots/{robotId}/{command}"
     headers = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {token}"
     }
-    payload = {
-        "command": command,
-        "params": parameters
-    }
+    if parameters == {}:
+        payload = {
+            "command": command
+        }
+    else:
+        payload = {
+            "command": command,
+            "params": parameters
+        }
+    
     response = requests.post(url=url, headers=headers, json=payload, timeout=5)
     
     print(f"Got status code {response.status_code}")
@@ -21,17 +42,87 @@ def send_command(base_url, token, command, robotId, parameters):
     
     #response.raise_for_status()
 
-    return response
 
-if __name__ == "__main__":
-    robot_url = "http://127.0.0.1:8000"
+def move_control():
+    base_url = "http://127.0.0.1:8000"
     token = "OneToken"
     command = "move"
     robotId = "R1234"
     parameters = {"taskId": 1234, "x": 11.11, "y": 22.22, "z": 33.33}
 
     try:
-        response = send_command(robot_url, token, command, robotId, parameters)
-        data = response.json()
+        send_command(base_url, token, command, robotId, parameters)  
     except requests.RequestException as e:
         print("error", e)
+
+
+def deliver_control():
+    base_url = "http://127.0.0.1:8000"
+    token = "OneToken"
+    command = "deliver"
+    robotId = "R1234"
+    parameters = {"taskId": 1234, "binId": 100}
+
+    try:
+        send_command(base_url, token, command, robotId, parameters)  
+    except requests.RequestException as e:
+        print("error", e)
+
+
+def pause_control():
+    base_url = "http://127.0.0.1:8000"
+    token = "OneToken"
+    command = "pause"
+    robotId = "R1234"
+    parameters = {}
+
+    try:
+        send_command(base_url, token, command, robotId, parameters)  
+    except requests.RequestException as e:
+        print("error", e)
+
+
+def resume_control():
+    base_url = "http://127.0.0.1:8000"
+    token = "OneToken"
+    command = "resume"
+    robotId = "R1234"
+    parameters = {}
+
+    try:
+        send_command(base_url, token, command, robotId, parameters)  
+    except requests.RequestException as e:
+        print("error", e)
+
+
+def abort_control():
+    base_url = "http://127.0.0.1:8000"
+    token = "OneToken"
+    command = "abort"
+    robotId = "R1234"
+    parameters = {}
+
+    try:
+        send_command(base_url, token, command, robotId, parameters)  
+    except requests.RequestException as e:
+        print("error", e)
+
+
+if __name__ == "__main__":
+    
+    while RUNNING:
+        command = input("Please enter your command id: ")
+        if command == COMMAND_MOVE:
+            move_control()
+        elif command == COMMAND_DELIVER:
+            deliver_control()
+        elif command == COMMAND_PAUSE:
+            pause_control()
+        elif command == COMMAND_RESUME:
+            resume_control()
+        elif command == COMMAND_ABORT:
+            abort_control()
+        elif command == 'q':
+            on_quit()
+        else:
+            print("Invalid input, please input number from 0 - 4 :)")
--- robot_side/func_commands/commands_get.py
+++ robot_side/func_commands/commands_get.py
@@ -17,40 +17,58 @@ def handle_move_command(robotId, command):
         y = parameters.get("y")
         z = parameters.get("z")
 
-        print("Command: ", cmd, "task_Id:", task_id, "x:", x, "y:", y, "z:", z)
+        print(f"Command: {cmd}. Task: {task_id} \n x: {x}, y: {y}, z: {z}")
 
         #robot move function and get results/
 
         result = {
             "status": "accepted",
-            "task_id": 1234,
+            "taskId": 1234,
             "plannedPath": [{"x": 0.0, "y": 0.0, "z": 0.0},
                             {"x": 3.0, "y": 2.0, "z": 0.0},
                             {"x": 6.0, "y": 4.0, "z": 0.0},
                             {"x": 10.0, "y": 5.0, "z": 0.0}]
         }
+
+        return jsonify(result), 200 if result["status"] == "accepted" else 400
     
     elif cmd == "deliver":
-        #TODO
         task_id = parameters.get("taskId")
         bin_id = parameters.get("binId")
+        print(f"Command: {cmd}, Task: {task_id}, Bin: {bin_id}")
+
+        result = {
+            "status": "accepted",
+            "taskId": 1235
+        }
+
+        return jsonify(result), 200 if result["status"] == "accepted" else 400
+
     elif cmd == "pause":
-        #TODO
+        print(f"Command {cmd}")
 
         result = {"status": "paused"}
+
+        return jsonify(result), 200 if result["status"] == "paused" else 400
+
     elif cmd == "resume":
-        #TODO
+        print(f"Command {cmd}")
 
         result = {"status": "resumed"}
-    elif cmd == "abort":
-        #TODO
-        result = {"status": "aborted"}
-    else:
-        #TODO - return the command is not allowed.
-        print("no process of this command")
 
-    
-    return jsonify(result), 200 if result["status"] == "accepted" else 400
+        return jsonify(result), 200 if result["status"] == "resumed" else 400
+
+    elif cmd == "abort":
+        print(f"Command {cmd}")
+
+        result = {"status": "aborted"}
+
+        return jsonify(result), 200 if result["status"] == "aborted" else 400
+
+    else:
+        
+        result = {"status": "This command is not defined."}
+        return jsonify(result), 422
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8000)
